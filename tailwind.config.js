9b1aea3d08391be5822e5a38694fbeaf45806da4
Airah Yusuff <airahyusuff@gmail.com>
2024-01-15 17:15:02 +0000

    style: set up tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+/** @type {import('tailwindcss').Config} */
+
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    fontFamily: {
+      sans: ["Poppins", "ui-sans-serif", "system-ui"],
+    },
+    extend: {
+      colors: {
+        transparent: "transparent",
+        success: "#138808",
+        danger: {
+          DEFAULT: "#FE0000",
+        },
+        domokun: "#3D2B1F",
+      }
+    },
+  },
+  plugins: [],
+}
+
